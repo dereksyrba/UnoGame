59b70fac0736b6a043fae19234e991c78120b285
dereksyrba <derek.syrba@hotmail.com>
2019-02-26 16:13:28 -0500

    Update UnoGUI.java

--- UnoGUI.java
+++ UnoGUI.java
@@ -14,14 +14,14 @@ import javax.swing.JOptionPane;
 
 /**
  * This class contains the main method to start the program and allow
- * the user to choose which game type to play
+ * the user to choose which game type to play.
  * 
  * @author Derek Syrba, John Frocillo, and Adrian Harrell
  */
 public class UnoGUI extends Application {
     
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(final Stage stage) throws Exception {
         
         Parent root = FXMLLoader.load(getClass().getResource("FXMLUno.fxml"));
         
@@ -34,29 +34,30 @@ public class UnoGUI extends Application {
     
     /**
      * The main method of the program that gives the user to option
-     * between two game types, then launches the game
+     * between two game types, then launches the game.
      * 
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         
         String[] a = {"Local Game", "AI Game"};
-        String input = (String)JOptionPane.showInputDialog( null,
+        String input = (String) JOptionPane.showInputDialog(null,
             "Select Game Mode:", "New Game", JOptionPane.QUESTION_MESSAGE,
             null, a, a[0]);
 		 	
-        if(input == null || input.length() == 0)
-                System.exit(0);
+        if (input == null || input.length() == 0) {
+			System.exit(0);
+		}
 
-        if(input.equals("Local Game"))	{
+        if (input.equals("Local Game"))	{
             launch(args);
         }
 
-        if(input.equals("AI Game"))	{
-            JOptionPane.showMessageDialog(null, "AI not supported in release 1.0");
+        if (input.equals("AI Game"))	{
+            JOptionPane.showMessageDialog(null, 
+            		"AI not supported in release 1.0");
             System.exit(0);
         }
        
     }
-    
 }
